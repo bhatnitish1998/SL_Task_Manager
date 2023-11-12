2cf7333014d2a5d65b11617bf380923bd0b08612
bhatnitish1998 <bhatnitish1998@gmail.com>
2023-11-12 19:26:02 +0530

    added network graphs and interface info and UI

--- UI/task_manager_app.py
+++ UI/task_manager_app.py
@@ -8,6 +8,7 @@ from task_manager_ui import Ui_main_window
 import sys
 from Data.get_resource_info import MemoryInfo
 from Data.get_all_data import SystemInfo
+from Data.network_data import NetworkInfo
 
 
 class TaskManagerWindow(QWidget, Ui_main_window):
@@ -37,11 +38,13 @@ class TaskManagerWindow(QWidget, Ui_main_window):
         self.cpu_timer.start()
 
         # set network timer
-        self.network_interval = 1000
+        self.network_interval = 500
         self.network_timer = QTimer()
         self.network_timer.setInterval(self.network_interval)
         self.network_timer.timeout.connect(self.update_network_info)
+        self.network_timer.timeout.connect(self.update_interface_info)
         self.network_timer.start()
+
 
         # set memory timer for update
         self.mem_interval = 500
@@ -66,12 +69,6 @@ class TaskManagerWindow(QWidget, Ui_main_window):
         data = memory.get_memory_usage()
         self.mem_usage_graph.setYRange(0, data["total"] / (1024 * 1024))
 
-        # add mem pie-chart
-        # self.mem_pie_chart = QPieSeries()
-        # self.swap_pie_chart = QPieSeries()
-        # self.vbox_memory.addWidget(self.mem_pie_chart)
-        # self.vbox_swap.addWidget(self.swap_pie_chart)
-
 
         # core graph
         self.core_graph = pg.PlotWidget()
@@ -88,7 +85,6 @@ class TaskManagerWindow(QWidget, Ui_main_window):
         # overall cpu graph
         self.cpu_overall_graph = pg.PlotWidget()
         self.hbox_overall_cpu.addWidget(self.cpu_overall_graph)
-        self.cpu_overall_x = []
         self.cpu_overall_graph.setLabel('left', "Utilization % ")
         self.cpu_overall_graph.setLabel('bottom', "Time in seconds")
         self.cpu_overall_graph.setYRange(0, 100)
@@ -96,6 +92,57 @@ class TaskManagerWindow(QWidget, Ui_main_window):
         self.cpu_graph_y = [0 for i in range(51)]
         pen = pg.mkPen(color=(0, 255, 0), width=5)
         self.cpu_line = self.cpu_overall_graph.plot(self.cpu_graph_x, self.cpu_graph_y, pen=pen)
+
+
+        # network transmit graph
+        self.network_transmit_graph = pg.PlotWidget()
+        self.hbox_transmit_graph.addWidget(self.network_transmit_graph)
+        self.network_transmit_graph.setLabel('left', "Transmit rate (KBps)")
+        self.network_transmit_graph.setLabel('bottom', "Time in seconds")
+        pen = pg.mkPen(color=(0, 255, 0), width=5)
+        self.network_transmit_x = [i / 1000 for i in range(-50 * self.network_interval, 1, self.network_interval)]
+        self.network_transmit_y =[0 for i in range(51)]
+        self.network_transmit_line = self.network_transmit_graph.plot(self.network_transmit_x,self.network_transmit_y,pen=pen)
+
+        # network receive graph
+        self.network_receive_graph = pg.PlotWidget()
+        self.hbox_receive_graph.addWidget(self.network_receive_graph)
+        self.network_receive_graph.setLabel('left', "Receive rate (KBps)")
+        self.network_receive_graph.setLabel('bottom', "Time in seconds")
+        pen = pg.mkPen(color=(0, 255, 0), width=5)
+        self.network_receive_x = [i / 1000 for i in range(-50 * self.network_interval, 1, self.network_interval)]
+        self.network_receive_y =[0 for i in range(51)]
+        self.network_receive_line = self.network_receive_graph.plot(self.network_receive_x,self.network_receive_y,pen=pen)
+
+        # add interface info
+        nw = NetworkInfo()
+        data = nw.network_info
+        cols = ["Interface", "IP", "MAC", "Send", "Receive"]
+        vbox_t = []
+        for i in range(len(cols)):
+            vbox_t.append(QVBoxLayout())
+            label = QLabel()
+            label.setText(cols[i])
+            font = QFont("Ubuntu", 12)
+            font.setBold(True)
+            label.setFont(font)
+            vbox_t[i].addWidget(label)
+
+        self.send_labels=[]
+        self.receive_labels=[]
+        for row in data:
+            for i in range(len(row)):
+                label = QLabel()
+                label.setText(row[i])
+                vbox_t[i].addWidget(label)
+                if i == 3:
+                    self.send_labels.append((label))
+                if i ==4:
+                    self.receive_labels.append(label)
+
+
+        for i in range(len(vbox_t)):
+            self.hbox_interfaces.addLayout(vbox_t[i])
 
         # add  diskinfo
         data = self.system_info.get_disk_data()
@@ -137,6 +184,15 @@ class TaskManagerWindow(QWidget, Ui_main_window):
             self.hbox_file_system.addLayout(vbox_t[i])
 
 
+    def update_interface_info(self):
+        nw = NetworkInfo()
+        data = nw.network_info
+        for i in range(len(data)):
+            for j in [3,4]:
+                if j == 3:
+                    self.send_labels[i].setText(data[i][j])
+                if j ==4:
+                    self.receive_labels[i].setText(data[i][j])
 
     def update_cpu_info(self):
         data = self.system_info.get_cpu_data()
@@ -198,7 +254,23 @@ class TaskManagerWindow(QWidget, Ui_main_window):
         self.mem_line.setData(self.mem_graph_x, self.mem_graph_y)
 
     def update_network_info(self):
-        pass
+        # update transmit graph
+        data = self.system_info.get_network_data()
+        transmit = data['total']['tx_KBps']
+        receive = data['total']['rx_KBps']
+        self.network_transmit_x.pop(0)
+        self.network_transmit_x.append(self.network_transmit_x[-1] + (self.network_interval / 1000))
+        self.network_transmit_y.pop(0)
+        self.network_transmit_y.append(transmit)
+        self.network_transmit_line.setData(self.network_transmit_x,self.network_transmit_y)
+
+
+        # update receive graph
+        self.network_receive_x.pop(0)
+        self.network_receive_x.append(self.network_receive_x[-1] + (self.network_interval / 1000))
+        self.network_receive_y.pop(0)
+        self.network_receive_y.append(receive)
+        self.network_receive_line.setData(self.network_receive_x,self.network_receive_y)
 
 
     def end_task(self):
